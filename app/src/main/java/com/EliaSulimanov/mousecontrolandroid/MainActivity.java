89cfb27f37b4acf64228e95617a42dc6b2528c8e
Elia <eliasulimanov@gmail.com>
2019-04-14 19:01:04 +0300

    improved test for key and added check if it is possible to create socket to the server

--- app/src/main/java/com/EliaSulimanov/mousecontrolandroid/MainActivity.java
+++ app/src/main/java/com/EliaSulimanov/mousecontrolandroid/MainActivity.java
@@ -1,12 +1,21 @@
 package com.EliaSulimanov.mousecontrolandroid;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,9 +33,23 @@ public class MainActivity extends AppCompatActivity {
                         if (!CheckKey(keyInputString)) {
                             Toast.makeText(getApplicationContext(), "Bad key", Toast.LENGTH_SHORT).show();
                         } else {
-                            Intent mouseIntent = new Intent(view.getContext(), TouchpadActivity.class);
-                            mouseIntent.putExtra("key", keyInputString);
-                            view.getContext().startActivity(mouseIntent);
+                            if(!CheckWifi(view.getContext()))
+                            {
+                                Toast.makeText(getApplicationContext(), "No WIFI", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                try{
+                                    new CheckIfServerAlive().execute(keyInputString);
+                                    Intent mouseIntent = new Intent(view.getContext(), TouchpadActivity.class);
+                                    mouseIntent.putExtra("key", keyInputString);
+                                    view.getContext().startActivity(mouseIntent);
+                                }
+                                catch (Exception e)
+                                {
+                                    Toast.makeText(getApplicationContext(), "No server found", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
                         }
                     }
                 });
@@ -49,9 +72,44 @@ public class MainActivity extends AppCompatActivity {
                 else
                     numOfDashes++;
         }
+        if(key.charAt(0) == '-' || key.charAt(key.length() - 1) == '-')
+            return false;
         if (numOfDashes != 1)
             return false;
         return true;
     }
 
+    /**
+     * Validate WIFI connected
+     * @param context current context
+     * @return false if no WIFI connected
+     */
+    private boolean CheckWifi(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    static class CheckIfServerAlive extends AsyncTask<String, Void, Boolean>
+    {
+        protected Boolean doInBackground(String... key)
+        {
+            Socket socket = null;
+            try {
+                socket = new Socket("192.168." + key[0].replace('-', '.'), 3333);
+                PrintWriter outWriter = new PrintWriter(socket.getOutputStream(), true);
+                socket.close();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Log.i("ES", e.toString());
+            }
+
+            return null;
+        }
+    }
 }
